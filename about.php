4dec0413d85cb0478c9a6421494176214905ad86
biligto6242 <biligto6242@yandex.ru>
2018-01-26 21:20:16 +0300

    Применение PHP как шаблонизатора

--- about.php
+++ about.php
@@ -5,15 +5,12 @@ $userAge  = 30;
 $userJob  = 'Администратор-специалист спутниковых сетей';
 $userCity = 'Москва';
 $userEmail= 'biligto6242@yandex.ru';
-
-echo ('<h3>Страница пользователя Билигто</h3>');
-
-print ("
-        
-        Имя                     $userName\n<br>\n
-        Возраст                 $userAge\n<br>\n
-        Адрес электронной почты $userEmail\n<br>\n
-        Город                   $userCity\n<br>\n
-        О себе                  $userJob\n<br>\n"
-        );
 ?>
+<div>
+        <h3>Страница пользователя Билигто</h3>
+        <p>Имя <?= $userName ?></p>
+        <p>Возраст <?= $userAge ?></p>
+        <p>Адрес электронной почты <a href="mailto:biligto6242@yandex.ru"><?= $userEmail ?></a></p>
+        <p>Город <?= $userCity ?></p>
+        <p>О себе <?= $userJob ?></p>
+</div>
