247112a189a136ee6c4cd018e7798ef299c4fd16
biligto6242 <biligto6242@yandex.ru>
2018-01-26 12:03:35 +0300

    Initial commit

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php
+
+$userName = 'Билигто';
+$userAge  = 30;
+$userJob  = 'Администратор-специалист спутниковых сетей';
+$userCity = 'Москва';
+$userEmail= 'biligto6242@yandex.ru';
+
+echo ('<h3>Страница пользователя Билигто</h3>');
+
+print ("
+        
+        Имя                     $userName\n<br>\n
+        Возраст                 $userAge\n<br>\n
+        Адрес электронной почты $userEmail\n<br>\n
+        Город                   $userCity\n<br>\n
+        О себе                  $userJob\n<br>\n"
+        );
+?>
