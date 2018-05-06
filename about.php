2b1e01f20679b98d354eacd2705b3d7a1babeb50
biligto6242 <biligto6242@yandex.ru>
2018-05-06 17:58:19 +0300

    $email

--- about.php
+++ about.php
@@ -10,7 +10,7 @@ $userEmail= 'biligto6242@yandex.ru';
         <h3>Страница пользователя Билигто</h3>
         <p>Имя <?= $userName ?></p>
         <p>Возраст <?= $userAge ?></p>
-        <p>Адрес электронной почты <a href="mailto:biligto6242@yandex.ru"><?= $userEmail ?></a></p>
+        <p>Адрес электронной почты <a href="mailto:<?= $userEmail ?>"><?= $userEmail ?></a></p>
         <p>Город <?= $userCity ?></p>
         <p>О себе <?= $userJob ?></p>
 </div>
